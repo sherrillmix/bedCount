ffeada132108578e9f2c5e635c2c6743cea00be2
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-03-07 14:48:34 -0500

    More tests

--- tests.bash
+++ tests.bash
@@ -48,11 +48,13 @@ echo "Testing bedCount"
 ./bedCount -$'\05' 2>/dev/null&& { echo "Weird arg did not fail"; exit 1; }
 ./bedCount -b $bedFile 2>/dev/null&& { echo "bed no bam did not fail"; exit 1; }
 ./bedCount -b $bedFile $sortBamFile  2>/dev/null|grep "	0$"|wc -l|grep '^2$' >/dev/null|| { echo "Unexpected bedCount output for bamFile with 0 left after border"; exit 1; }
+./bedCount -b ${bedFile2}.NOTAREALFILE $sortBamFile $sortBamFile2 -B 0 -G 2>/dev/null&& { echo "non-existent bed did not fail"; exit 1; }
 ./bedCount -b $bedFile $sortBamFile $sortBamFile2 -B 0 2>/dev/null|head -1|grep "chr1:20-20	chr1:20-20	1	1" >/dev/null|| { echo "Unexpected output with 0 border"; exit 1; }
 ./bedCount -b $bedFile $sortBamFile $sortBamFile2 -B 0 -s 2>/dev/null|head -1|grep "chr1:20-20	chr1:20-20	2	1" >/dev/null|| { echo "Unexpected output with 0 border and not require pair"; exit 1; }
 ./bedCount -b $bedFile $sortBamFile $sortBamFile2 -B 0 -s -G 2>/dev/null|tail -1|grep "GLOBAL	GLOBAL	2	1" >/dev/null|| { echo "Unexpected output with 0 border and not require pair and global"; exit 1; }
 ./bedCount -b $bedFile2 $sortBamFile $sortBamFile2 -B 0 -s -G 2>/dev/null|tail -1|grep "GLOBAL	GLOBAL	3	2" >/dev/null|| { echo "Unexpected output with 0 border and not require pair and global"; exit 1; }
 ./bedCount -b $bedFile2 $sortBamFile $sortBamFile2 -B 0 -G 2>/dev/null|tail -1|grep "GLOBAL	GLOBAL	1	1" >/dev/null|| { echo "Unexpected output with 0 border and pair and global"; exit 1; }
+./bedCount -b $bedFile2 $sortBamFile $sortBamFile2 -B 0 -G -t 2 2>/dev/null|tail -1|grep "GLOBAL	GLOBAL	1	1" >/dev/null|| { echo "Unexpected output 2 threads"; exit 1; }
 
 echo "Testing bam2depth"
 ./bam2depth 2>/dev/null && { echo "Missing files did not fail"; exit 1; }
