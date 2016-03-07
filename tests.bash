fd7e77badced1b8fffecab22d0bdb18d5efc4370
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-03-07 08:53:20 -0500

    Weird args

--- tests.bash
+++ tests.bash
@@ -4,6 +4,10 @@ set -e
 
 ./bedCount 2>/dev/null && { echo "Missing files did not fail"; exit 1; }
 ./bedCount -h |grep Usage|| { echo "-h did not generate usage"; exit 1; }
+./bedCount -Z && { echo "Weird arg did not fail"; exit 1; }
+./bedCount -$'\05' && { echo "Weird arg did not fail"; exit 1; }
 
 ./bam2depth 2>/dev/null && { echo "Missing files did not fail"; exit 1; }
 ./bam2depth -h |grep Usage|| { echo "-h did not generate usage"; exit 1; }
+./bam2depth -Z && { echo "Weird arg did not fail"; exit 1; }
+./bam2depth -$'\05' && { echo "Weird arg did not fail"; exit 1; }
